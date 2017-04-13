35b54dd5b60adea673ac05e8c690c1487298e15a
R-90 <Ross.Galloway@vmsuk.com>
2017-04-13 09:35:07 +0100

    Cleaned up unused light node values

--- FormsSample/FormsSample/Joystick.cs
+++ FormsSample/FormsSample/Joystick.cs
@@ -13,11 +13,11 @@ namespace FormsSample
     public class Joystick : Application
     {
         bool movementEnabled;
+        
         Scene scene;
         Camera camera;
         Node mainNode;
         Octree octree;
-
 
         [Preserve]
         public Joystick(ApplicationOptions options = null) : base(options) { }
@@ -51,20 +51,24 @@ namespace FormsSample
             //Input.SubscribeToTouchEnd(OnTouched);
 
             var cache = ResourceCache;
-            Vector3 pButtonScale = new Vector3(2, 1, 0.5f);
+            Vector3 pButtonScale = new Vector3(1.5f, 0.5f, 0.25f);
+            Vector3 stickScale = new Vector3(1, 1, 0.5f);
             scene = new Scene();
             octree = scene.CreateComponent<Octree>();
 
             mainNode = scene.CreateChild();
             mainNode.Position = new Vector3(0, 0, 10);
-            Node stickNode = mainNode.CreateChild();
+            Node stickNode = mainNode.CreateChild(name: "stick");
             Sphere stick = stickNode.CreateComponent<Sphere>();
             stick.Color = Color.White;
+            
+            stickNode.Scale = stickScale;
 
             Node pUpNode = mainNode.CreateChild(name: "panUp");
             Node pDownNode = mainNode.CreateChild(name: "panDown");
             Node pRightNode = mainNode.CreateChild(name: "panRight");
             Node pLeftNode = mainNode.CreateChild(name: "panLeft");
+            Node backgroundNode = mainNode.CreateChild(name: "background");
 
             Pyramid pUp = CreatePyramid(pUpNode);
             Pyramid pDown = CreatePyramid(pDownNode);
@@ -76,17 +80,14 @@ namespace FormsSample
             pRightNode.Scale = pButtonScale;
             pLeftNode.Scale = pButtonScale;
 
-            pUpNode.Position = new Vector3(0, 2, 0);
-            pDownNode.Position = new Vector3(0, -2, 0);
-            pRightNode.Position = new Vector3(2, 0, 0);
-            pLeftNode.Position = new Vector3(-2, 0, 0);
+            pUpNode.Position = new Vector3(0, 3.75f, 0);
+            pDownNode.Position = new Vector3(0, -3.75f, 0);
+            pRightNode.Position = new Vector3(3f, 0, 0);
+            pLeftNode.Position = new Vector3(-3f, 0, 0);
 
-            //pRightNode.Rotation = new Quaternion(Vector3.UnitZ, 90);
-            //pLeftNode.Rotation = new Quaternion(Vector3.UnitZ, -90);
-            //pDownNode.Rotation = new Quaternion(Vector3.UnitZ, 180);
-
-            //var plane = baseNode.CreateComponent<StaticModel>();
-            //plane.Model = ResourceCache.GetModel("Models/Plane.mdl");
+            pRightNode.Rotation = new Quaternion( 0,  0, -90);
+            pLeftNode.Rotation = new Quaternion( 0, 0, 90);
+            pDownNode.Rotation = new Quaternion(0, 0, 180);
 
             var cameraNode = scene.CreateChild("camera");
             camera = cameraNode.CreateComponent<Camera>();
@@ -95,16 +96,64 @@ namespace FormsSample
             var light = lightNode.CreateComponent<Light>();
             light.LightType = LightType.Directional;
             lightNode.SetDirection(new Vector3(0,0,1));
-            light.Range = 100;
-            //light.Brightness = 1.3f;
-
-            
         }
+
+        
 
         void SetupViewport()
         {
             var renderer = Renderer;
             renderer.SetViewport(0, new Viewport(Context, scene, camera, null));
         }
+    }
+
+    // moving stick component to it's own class.
+    public class Stick : Component
+    {
+        Node stickNode;
+        Color color;
+        float lastUpdateValue;
+
+
+        public Vector3 Value
+        {
+            get { return stickNode.Position; }
+            set { stickNode.Position = new Vector3( value.X, value.Y, 0 ); }
+        }
+
+        public Stick(Color color)
+        {
+            this.color = color;
+        }
+
+        public override void OnAttachedToNode(Node node)
+        {
+            stickNode = node.CreateChild();
+            stickNode.Scale = new Vector3(1, 1, 0.5f); 
+            Sphere sphere = stickNode.CreateComponent<Sphere>();
+            sphere.Color = color;
+
+            base.OnAttachedToNode(node);
+        }
+
+
+        protected override void OnUpdate(float timeStep)
+        {
+            var input = Application.Current.Input;
+            if (input.NumTouches > 0)
+            {
+                TouchState state = input.GetTouch(0);
+                var touchPosition = state.Position;
+                //stickPosX = touchPosition.X;
+                //stickPos
+            }
+
+            base.OnUpdate(timeStep);
+            var stickPos = stickNode.Position;
+
+            stickNode.Position = new Vector3(stickPos.X, stickPos.Y, 0);
+        }
+
+        
     }
 }
